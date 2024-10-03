a8ee7af84f5ce41740905c77ab4b285cd0829e67
robiulhasananim <mdrobiulhasananim@gmail.com>
2024-10-03 12:01:49 +0600

    add color category photo to product app

--- products/models.py
+++ products/models.py
@@ -7,6 +7,9 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(max_length=50)
+    colors = models.JSONField(blank=True, null=True)  # Can store multiple colors 
+    photo = models.ImageField(upload_to='products/photos/', blank=True, null=True)
 
     def __str__(self):
         return self.name
